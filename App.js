7b3b1159e3daa8c3a5c8f17c5d54c01e8f224a79
rayleigh420 <nhatduy0409@gmail.com>
2023-06-08 15:18:20 +0700

    Try some component and event

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, TextInput, Touchable, View } from 'react-native';
 import Hello from './src/components/Hello';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="">
       <Hello />
-      <StatusBar style="auto" />
+      <Text onPress={() => { Alert.alert("Hello") }}>Press</Text>
+      <TextInput className="border-black border-[1px]" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -2,7 +2,7 @@ import { Text } from "react-native"
 
 const Hello = () => {
     return (
-        <Text>Hello everyone. My name is Le Nhat Duy</Text>
+        <Text className="text-red-500 ">Hello everyone. My name is Le Nhat Duy.</Text>
     )
 }
 export default Hello
