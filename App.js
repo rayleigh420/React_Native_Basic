bd4c3494d5c40c4a48b7dddd73521844099c637a
rayleigh420 <nhatduy0409@gmail.com>
2023-06-11 09:38:04 +0700

    Try to fix error with react naviagtion drawer and try bottom tab

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Todo from './src/components/Todo';
 import Screen1 from './src/Screens/Screen1';
 import Screen2 from './src/Screens/Screen2';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator();
+// const Drawer = createDrawerNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        {/* <Stack.Screen name='Todo List' component={Todo} /> */}
-        <Stack.Screen name='Screen 1' component={Screen1} />
-        <Stack.Screen name='Screen 2' component={Screen2} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name='Screen 1' component={Screen1} />
+        <Tab.Screen name='Screen 2' component={Screen2} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
